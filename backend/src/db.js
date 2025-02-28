c24226cd2b21d070e38e776deb0727e54410df08
Gaurav Awasthi <85209604+Program-code-learner@users.noreply.github.com>
2025-02-28 13:04:05 +0530

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   console.log(process.env.MONGODB_URI);
 
-  const connection = await mongoose.connect(process.env.MONGODB_URI);
+  const connection = await mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://gauravawasthigehu:gaurav123@cluster0.la480.mongodb.net/");
   
   console.log('MongoDB Connected Successfully');
   return connection;
