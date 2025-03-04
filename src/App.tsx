6aa23da2a68efc99262c7e94e102a9fdb6ed579e
Gaurav Awasthi <85209604+Program-code-learner@users.noreply.github.com>
2025-03-04 15:04:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,57 @@
+// import "./App.css";
+// import { Route, Routes } from "react-router-dom";
+// import Home from "./components/pages/Home";
+// import Login from "./components/pages/Login";
+// import Signup from "./components/pages/Signup";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import MovieDescription from "./components/pages/DetailPage";
+// import { AuthProvider } from "./components/pages/AuthProvider";
+// import { ProtectedRoute } from "./ProtectedRoute";
+// import { PublicRoute } from "./PublicRoutes";
+
+// function App() {
+//   const queryClient = new QueryClient();
+  
+//   return (
+//     <AuthProvider>
+//       <QueryClientProvider client={queryClient}>
+//         <Routes>
+//           <Route 
+//             path="/" 
+//             element={
+//               <PublicRoute>
+//                 <Login />
+//               </PublicRoute>
+//             } 
+//           />
+//           <Route 
+//             path="/signup" 
+//             element={
+//               <PublicRoute>
+//                 <Signup />
+//               </PublicRoute>
+//             } 
+//           />
+//           <Route path="/movie/:id" element={<MovieDescription />} />
+//           <Route
+//             path="/home"
+//             element={
+//               <ProtectedRoute>
+//                 <Home />
+//               </ProtectedRoute>
+//             }
+//           />
+//         </Routes>
+//       </QueryClientProvider>
+//     </AuthProvider>
+//   );
+// }
+
+// export default App;
+
+
+
+
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
@@ -6,8 +60,6 @@ import Signup from "./components/pages/Signup";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MovieDescription from "./components/pages/DetailPage";
 import { AuthProvider } from "./components/pages/AuthProvider";
-import { ProtectedRoute } from "./ProtectedRoute";
-import { PublicRoute } from "./PublicRoutes";
 
 function App() {
   const queryClient = new QueryClient();
@@ -16,31 +68,10 @@ function App() {
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
         <Routes>
-          <Route 
-            path="/" 
-            element={
-              <PublicRoute>
-                <Login />
-              </PublicRoute>
-            } 
-          />
-          <Route 
-            path="/signup" 
-            element={
-              <PublicRoute>
-                <Signup />
-              </PublicRoute>
-            } 
-          />
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/movie/:id" element={<MovieDescription />} />
-          <Route
-            path="/home"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </QueryClientProvider>
     </AuthProvider>
